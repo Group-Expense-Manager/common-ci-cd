12b5537e19393139de8b87690292fa2aa4826099
Jakub Spolnik <lawos231@gmail.com>
2024-12-18 17:46:57 +0100

    GEM-243 | Fix for jira regex

--- .github/actions/jira-ticket-validator.js
+++ .github/actions/jira-ticket-validator.js
@@ -4,12 +4,12 @@ function validateJiraTicket(jiraTicketRegex, input, fieldName) {
     if (jiraTicketRegex.test(input)) {
         console.log(`${fieldName} is valid.`);
     } else {
-        console.error(`${fieldName} is invalid. It should have a Jira ticket number in the format "GEM-21". (${input})`);
+        console.error(`${fieldName} is invalid. It should have a Jira ticket number in the format "GEM-21", "GEM-234-fix", or "GEM-234_fix". (${input})`);
         core.setFailed('Jira ticket number validation failed.');
     }
 }
 
-const jiraTicketRegex = /^GEM-\d+(\s\|\s\w.*)?$/;
+const jiraTicketRegex = /^GEM-\d+[-_]?\w*$/;
 
 const branchName = process.env.GITHUB_HEAD_REF || '';
 const prTitle = process.argv[2];
